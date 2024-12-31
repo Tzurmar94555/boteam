38f8ea39f1b858722c6690293cd6be5648d7e6dd
seanwang-ab45 <c112156111@nkust.edu.tw>
2024-12-31 15:46:52 +0800

    新增 test3.py

---
+++ test3.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+@bot.command()
+async def send_image(ctx):
+    await ctx.send(file=discord.File('https://pbs.twimg.com/media/GLCr8DEWgAACuCX.jpg:large'))
+
+bot.run('')
+print("照片已發送")
